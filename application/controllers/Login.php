5269a82c6b8ace897dfcdbde32a21b5ecdc341eb
maurisaarimbi <caca.maurisa@gmail.com>
2022-12-21 20:55:35 +0700

    fix login controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,55 +12,8 @@ class Login extends CI_Controller {
 
     function index()
 	{
-		// $this->load->view('v_register');
         $data['title'] = "Login";
         $data['title_page'] = "Login";
         $this->templateuser->disp_login('login', $data);
-		// if($this->session->userdata('is_login') == true) {
-		// 	if ($this->session->userdata('akses')=='admin') {
-		// 		redirect('admin/home');
-		// 	}
-		// 	elseif ($this->session->userdata('akses')=='user') {
-		// 		redirect('home');
-		// 	}
-	  	// }
 	}
-
-	// function process()
-	// {
-	// 	$username = $this->input->post('username');
-	// 	$password = $this->input->post('password');
-	// 	$akses = $this->input->post('akses');
-	// 	if(($this->M_auth->login_user($username,$password,$akses))){
-	// 		if ($this->session->userdata('akses')=='admin') {
-	// 			redirect('admin/home');
-	// 		}
-	// 		elseif ($this->session->userdata('akses')=='user') {
-	// 			redirect('home');
-	// 		}
-	// 	}
-	// 	// else if (($this->M_auth->login_user($username,$password,$akses))){
-	// 	// 	redirect('home');
-	// 	// }
-	// 	else
-	// 	{
-	// 		// $this->session->set_flashdata('error','Username & Password salah');
-    //         $this->session->set_flashdata('msg','
-    //             <div class="alert alert-danger fade show position-fixed top-3" role="alert">
-	// 			    <strong>Failed!</strong> Login is failed.
-	// 			    <button type="button" class="btn-close ml-auto" data-bs-dismiss="alert" aria-label="Close"></button>
-	// 		    </div>
-    //             ');
-	// 		redirect('login');
-	// 	}
-	// }
-
-	// public function logout()
-	// {
-	// 	$this->session->unset_userdata('username');
-	// 	$this->session->unset_userdata('name');
-	// 	$this->session->unset_userdata('akses');
-	// 	$this->session->unset_userdata('is_login');
-	// 	redirect('login');
-	// }
 }
