10a503553083a700a2df0afa1a96a74d58662236
maurisaarimbi <caca.maurisa@gmail.com>
2022-12-31 10:12:09 +0700

    fix

--- application/views/dashboard/tambahuser.php
+++ application/views/dashboard/tambahuser.php
@@ -1,8 +1,5 @@
 <div class="col-xl-12 col-md-6">
     <div class="card">
-        <div class="card-header bg-info">
-
-        </div>
         <div class="card-body">
             <div class="row">
                 <div class="col-lg-12">
